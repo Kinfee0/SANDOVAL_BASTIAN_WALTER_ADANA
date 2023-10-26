931d6f54ca4d529f5404bf8ba1d48eda9c16107e
Kinfee0 <bastiansandovaal@gmail.com>
2023-10-26 00:34:13 -0300

    first commit

---
+++ src/com/backend/parcial/dao/H2Conecction.java
@@ -0,0 +1,14 @@
+package com.backend.parcial.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class H2Conecction {
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("org.h2.Driver");
+        return DriverManager.getConnection(" jdbc:h2:~/test", "sa", "sa");
+    }
+}
+
